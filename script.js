3f3c50a0cb1b92491977a4b57d6f50ca7e0b72bd
Hardik Kajne <hardikkajne@gmail.com>
2026-06-13 12:43:53 +0530

    added few sections

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// Menu button logic
 let menu = document.querySelector('.menu-logo');
 let navlinks = document.querySelector('.nav-links');
 menu.addEventListener('click',()=>{
     navlinks.classList.toggle('nav-links-active');
 })
+
+    
